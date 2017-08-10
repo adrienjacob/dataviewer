b0c6112dc39887274b1e36218987ed2a09b4c3f5
Bastian Zagar <zagar@aixdesign.net>
2017-08-10 19:11:38 +0200

    Using parent getters and setters

--- Classes/Domain/Model/RecordValue.php
+++ Classes/Domain/Model/RecordValue.php
@@ -11,7 +11,7 @@ namespace MageDeveloper\Dataviewer\Domain\Model;
  * @copyright   Magento Developers / magedeveloper.de <kontakt@magedeveloper.de>
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class RecordValue extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
+class RecordValue extends AbstractModel
 {
 	/**
 	 * Value Content
