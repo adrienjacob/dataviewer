e3eb1458b93512d1dc0361f57a39e013249a66a1
Bastian Zagar <zagar@aixdesign.net>
2017-10-20 07:59:59 +0200

    RealUrl Configuration and Custom User Element

--- ext_tables.php
+++ ext_tables.php
@@ -213,6 +213,23 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['extTablesInclusion-PostProc
  */
 $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][] = 'MageDeveloper\\Dataviewer\\Hooks\\ToolbarItem';
 
+
+/***********************************
+ * Custom User Element for
+ * Manipulating Inline Outputs
+ ***********************************/
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry']["user"] = [
+    "nodeName" => "user",
+    "priority" => "1",
+    "class" => "MageDeveloper\\Dataviewer\\Form\\Element\\UserElement",
+];
+
+
+/***********************************
+ * RealUrl Configuration
+ ***********************************/
+$GLOBALS["TYPO3_CONF_VARS"]["SC_OPTIONS"]["ext/realurl/class.tx_realurl_autoconfgen.php"]["extensionConfiguration"][$_EXTKEY] = "MageDeveloper\\Dataviewer\\RealUrl\\RealUrlConfiguration->getRealUrlConfiguration";
+
 /***********************************
  * Backend Stuff
  ***********************************/
