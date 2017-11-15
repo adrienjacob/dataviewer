78d5db3d22549ebcafb3ca479ab058df43ec7185
Bastian Zagar <zagar@aixdesign.net>
2017-11-15 20:55:28 +0100

    [TASK] Improved Filter behaviour

--- Classes/Controller/FilterController.php
+++ Classes/Controller/FilterController.php
@@ -46,6 +46,13 @@ class FilterController extends AbstractController
 	 */
 	protected $fieldRepository;
 
+    /**
+     * Prepared Filters
+     *
+     * @var array
+     */
+    protected $filters = [];
+
 	/**
 	 * Index Action
 	 * Displays the search form and
@@ -86,62 +93,61 @@ class FilterController extends AbstractController
 		if(!$this->_checkTargetUid())
 			$this->_redirectToPid();
 
-		if ($this->request->hasArgument("filters"))
-		{
-			// Prepare Filters
-			$filtersFromPost = $this->request->getArgument("filters");
-			$merge = false; // Merge Filters, this is on our todo list
-			$selected = [];
+        if ($this->request->getMethod() == "POST") {
 
-			foreach($filtersFromPost as $_fieldId=>$_fArray)
-			{
-				foreach($_fArray as $key=>$_id)
-				{
-					if ($selectedOption = $this->_getOptionById($_id))
-						$selected[] = $selectedOption;
-					else
-						if($selectedOption = $this->_getOptionById($key))
-						{
-							// We only add the filter, if we received a useful value
-							if($_id != "")
-							{
-								$selectedOption["field_value"] = $_id;
-								$selected[] = $selectedOption;
-							}
-						}
-						else
-						{
-							// Option was not found in current filter,
-							// so we determine the setting, if we can
-							// merge it anyway with the previous selected
-							if($merge === true)
-							{
-								// We need to security check the selected option
-								$selectedOption["field_value"] = $_id;
-								$selected[] = $selectedOption;
-							}
-						}
-				}
+            $selected = [];
+            $merge = false; // TODO: Make configurable?
+            if($this->request->hasArgument("filters")) {
 
-			}
+                // Prepare Filters
+                $filtersFromPost = $this->request->getArgument("filters");
 
-			$previousSelected = $this->filterSessionService->getSelectedOptions();
-			$selectedOptions = $this->_prepareSelectedOptionsArray($previousSelected, $selected, $merge);
+                foreach ($filtersFromPost as $_fieldId => $_fArray) {
+                    foreach ($_fArray as $key => $_id) {
+                        if ($selectedOption = $this->_getOptionById($_id))
+                            $selected[] = $selectedOption;
+                        else
+                            if ($selectedOption = $this->_getOptionById($key)) {
+                                // We only add the filter, if we received a useful value
+                                if ($_id != "") {
+                                    $selectedOption["field_value"] = $_id;
+                                    $selected[] = $selectedOption;
+                                }
+                            } else {
+                                // Option was not found in current filter,
+                                // so we determine the setting, if we can
+                                // merge it anyway with the previous selected
+                                if ($merge === true) {
+                                    // We need to security check the selected option
+                                    $selectedOption["field_value"] = $_id;
+                                    $selected[] = $selectedOption;
+                                }
+                            }
+                    }
 
-			/////////////////////////////////////////////////////////////////
-			// Signal-Slot for the post-processing of the selected options //
-			/////////////////////////////////////////////////////////////////
-			$this->signalSlotDispatcher->dispatch(
-				__CLASS__,
-				"postProcessSelectedOptions",
-				[
-					&$selectedOptions,
-					&$this,
-				]
-			);
+                }
 
-			$this->filterSessionService->setSelectedOptions($selectedOptions);
-		}
+            }
+
+
+            $previousSelected = $this->filterSessionService->getSelectedOptions();
+            $selectedOptions = $this->_prepareSelectedOptionsArray($previousSelected, $selected, $merge);
+
+            /////////////////////////////////////////////////////////////////
+            // Signal-Slot for the post-processing of the selected options //
+            /////////////////////////////////////////////////////////////////
+            $this->signalSlotDispatcher->dispatch(
+                __CLASS__,
+                "postProcessSelectedOptions",
+                [
+                    &$selectedOptions,
+                    &$this,
+                ]
+            );
+
+            $this->filterSessionService->setSelectedOptions($selectedOptions);
+
+        }
 
 		$this->_redirectToPid();
 	}
@@ -202,37 +208,42 @@ class FilterController extends AbstractController
 		}
 	}
 
-	/**
-	 * Gets the filters from the plugin settings
-	 *
-	 * @return array
-	 */
-	protected function _getFilters()
-	{
-		$filters = $this->filterSettingsService->getFilters();
+    /**
+     * Gets the filters from the plugin settings
+     *
+     * @return array
+     */
+    protected function _getFilters()
+    {
+        if(!$this->filters || empty($this->filters)) {
 
-		// Fill in session data
-		foreach($filters as $i=>$_filter)
-		{
-			$fieldId = $_filter["field_id"];
-			$filters[$i]["is_active"] = false;
-			$filterType = $_filter["filter_type"];
+            $filters = $this->filterSettingsService->getFilters();
 
-			foreach($_filter["options"] as $j=>$_option)
-			{
-				$optionId = $_option["id"];
-				$optionSelected = $this->filterSessionService->checkIsSelected($fieldId, $optionId);
-				$filters[$i]["options"][$j]["selected"] = $optionSelected;
-				$filters[$i]["options"][$j]["filter_type"] = $filterType;
+            // Fill in session data
+            foreach($filters as $i=>$_filter)
+            {
+                $fieldId = $_filter["field_id"];
+                $filters[$i]["is_active"] = false;
+                $filterType = $_filter["filter_type"];
 
-				if ($optionSelected)
-					$filters[$i]["is_active"] = true;
+                foreach($_filter["options"] as $j=>$_option)
+                {
+                    $optionId = $_option["id"];
+                    $optionSelected = $this->filterSessionService->checkIsSelected($fieldId, $optionId);
+                    $filters[$i]["options"][$j]["selected"] = $optionSelected;
+                    $filters[$i]["options"][$j]["filter_type"] = $filterType;
 
-			}
-		}
+                    if ($optionSelected)
+                        $filters[$i]["is_active"] = true;
 
-		return $filters;
-	}
+                }
+            }
+
+            $this->filters = $filters;
+        }
+
+        return $this->filters;
+    }
 
 	/**
 	 * Gets an according option setting by a given id hash
@@ -266,27 +277,118 @@ class FilterController extends AbstractController
 	 */
 	protected function _prepareSelectedOptionsArray(array $previousSelectedOptions = [], array $currentSelectedOptions = [], $merge = false)
 	{
-		$selectedOptions = [];
+        $selectedOptions = [];
 
-		foreach($previousSelectedOptions as $i=>$_prvOpt)
-			foreach($currentSelectedOptions as $j=>$_curOpt)
-			{
-				if(($_prvOpt["field_id"] == $_curOpt["field_id"]) && $merge === false)
-				{
-					// Clean previous array
-					unset($previousSelectedOptions[$i]);
-				}
+        // We remove filters, that have no filter_field
+        foreach($previousSelectedOptions as $i=>$_prvOpt)
+            if($_prvOpt["filter_field"] == "")
+                unset($previousSelectedOptions[$i]);
 
-				if($_curOpt["filter_field"] == "")
-				{
-					// Resetting the filter 
-					unset($currentSelectedOptions[$j]);
-				}
-			}
+        foreach($currentSelectedOptions as $i=>$_curOpt)
+            if($_curOpt["filter_field"] == "")
+                unset($currentSelectedOptions[$i]);
 
-		$selectedOptions = array_merge($previousSelectedOptions, $currentSelectedOptions);
-		return $selectedOptions;
+
+        foreach($previousSelectedOptions as $i=>$_prvOpt)
+            foreach($currentSelectedOptions as $j=>$_curOpt)
+            {
+                if(($_prvOpt["field_id"] == $_curOpt["field_id"]) && $merge === false)
+                {
+                    // Clean previous array
+                    unset($previousSelectedOptions[$i]);
+                }
+
+            }
+
+        $selectedOptions = array_merge($previousSelectedOptions, $currentSelectedOptions);
+
+        // We need to group the selected options by their field
+        $selectedOptionsGrouped = $this->_groupSelectedOptions($selectedOptions);
+
+        $options = [];
+        foreach($selectedOptionsGrouped as $_groupId=>$_group) {
+            $adjustedOptions = $this->_createAdjustedFilterCombinationForFilters($_group);
+            $options = array_merge($options, $adjustedOptions);
+        }
+
+        return $options;
 	}
+
+    /**
+     * Creates correct filter rules for grouped selected options
+     *
+     * @param array $selectedOptionsGrouped
+     * @return array
+     */
+    protected function _createAdjustedFilterCombinationForFilters(array $selectedOptionsGrouped)
+    {
+        $filters = $this->_getFilters();
+        $groupFilterCombinations = [];
+        foreach($filters as $_filter) {
+            $groupFilterCombinations[$_filter["field_id"]] = $_filter["group_filter_combination"];
+        }
+
+        // We need to create a correct working filter_condition for all the filters in our array
+        if(count($selectedOptionsGrouped) == 1) {
+
+            // Filter combination of the initial element
+            $fieldId = $selectedOptionsGrouped[0]["field_id"];
+            $filterCombination = $groupFilterCombinations[$fieldId];
+            $selectedOptionsGrouped[0]["filter_combination"] = "{$filterCombination} (...)";
+
+        } elseif (count($selectedOptionsGrouped) == 2) {
+
+            // Filter Combination of the Starting Element
+            $fieldIdStart = $selectedOptionsGrouped[0]["field_id"];
+            $filterCombinationStart = $groupFilterCombinations[$fieldIdStart];
+            $selectedOptionsGrouped[0]["filter_combination"] = "{$filterCombinationStart} (...";
+
+            // Filter Combination of the Last Element
+            $filterCombinationEnd = $selectedOptionsGrouped[1]["filter_combination"];
+            $selectedOptionsGrouped[1]["filter_combination"] = "{$filterCombinationEnd} ...)";
+
+        } elseif (count($selectedOptionsGrouped) > 2) {
+            for($i = 0; $i<count($selectedOptionsGrouped);$i++) {
+                if($i == 0) {
+
+                    // FIRST ONE HAS TO START WITH 'AND (...' TO OPEN
+                    $fieldIdStart = $selectedOptionsGrouped[$i]["field_id"];
+                    $filterCombinationStart = $groupFilterCombinations[$fieldIdStart];
+                    $selectedOptionsGrouped[$i]["filter_combination"] = "{$filterCombinationStart} (...";
+
+                } elseif($i == count($selectedOptionsGrouped) - 1) {
+
+                    // LAST ONE HAS TO END WITH A ) TO CLOSE ALL
+                    $filterCombinationOfLast = $selectedOptionsGrouped[count($selectedOptionsGrouped)-1]["filter_combination"];
+                    $selectedOptionsGrouped[$i]["filter_combination"] = "{$filterCombinationOfLast} ...)";
+
+                } else {
+                    $currentFilterCombination = $selectedOptionsGrouped[$i]["filter_combination"];
+                    $selectedOptionsGrouped[$i]["filter_combination"] = "{$currentFilterCombination} ...";
+                }
+            }
+        }
+
+        return $selectedOptionsGrouped;
+    }
+
+    /**
+     * Groups selected options by field
+     *
+     * @param array $selectedOptions
+     * @param string $groupField
+     * @return array
+     */
+    protected function _groupSelectedOptions($selectedOptions, $groupField = "field_id")
+    {
+        $grouped = [];
+        foreach($selectedOptions as $i=>$_sel) {
+            $fieldId = trim($_sel[$groupField]);
+            $grouped[$fieldId][] = $_sel;
+        }
+
+        return $grouped;
+    }
 
 	/**
 	 * initializeView
